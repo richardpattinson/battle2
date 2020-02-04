8611ede8bbc25db3725fa0b84a946ec2bd4b0f57
Josh Blumberg <jlblumberg@gmail.com>
2020-02-04 17:02:45 +0000

    Red second feature test, start view structure

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -6,3 +6,13 @@ feature 'Testing infrastructure' do
     expect(page).to have_content("Testing infrastructure working!")
   end
 end
+
+feature '2 players start game' do
+  scenario 'Can add two player names to a form' do
+    visit('/')
+    fill_in :player1_name, with: 'Richard'
+    fill_in :player2_name, with: 'Josh'
+    click_button 'Submit'
+    expect(page).to have_content("Richard vs Josh")
+  end
+end
